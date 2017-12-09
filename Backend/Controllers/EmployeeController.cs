3092cc24fb86315986bb6499e05d0a83981d7037
Joel2508 <Joelarias2508@gmail.com>
2017-12-09 06:27:51 +0100

    New app

---
+++ Backend/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Backend.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ViewAll()
+        {
+            return View(GetEmployee());
+        }
+        IEnumerable<Employee> GetEmployee()
+        {
+            using (var db = new BDModel())
+            {
+                return db.Employees.ToList<Employee>();
+            }
+        }
+
+    }
+
+}
